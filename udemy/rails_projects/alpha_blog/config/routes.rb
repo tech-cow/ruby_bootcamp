5175648a9300d93c287581c6e8d25f85c9d38c84
Yu <junior147147>
2019-05-09 23:09:47 -0700

    auto push at 2019-05-09 23:09:47.079866

--- udemy/rails_projects/alpha_blog/config/routes.rb
+++ udemy/rails_projects/alpha_blog/config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # welcome#home action
-  get 'welcome/home', to: 'welcome#home'
-  get 'welcome/about', to: 'welcome#about'
-
-
 end
