7dabd12a8f752be0396b000ba3279d8d8076f54d
Yu <junior147147>
2019-05-14 21:26:34 -0700

    auto push at 2019-05-14 21:26:34.364161

--- udemy/rails_projects/alpha_blog/db/schema.rb
+++ udemy/rails_projects/alpha_blog/db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_10_063210) do
+ActiveRecord::Schema.define(version: 2019_05_10_063712) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
